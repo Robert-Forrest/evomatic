63b82befc056d55ad2cf9f9b8ca4079f72130f85
Robert Forrest <robertforrest@live.com>
2022-11-14 12:36:09 +0000

    Added a test to check constraints are followed

--- tests/test_mass_maximisation.py
+++ tests/test_mass_maximisation.py
@@ -8,3 +8,20 @@ def test_mass_maximisation():
     history = evolver.evolve()
 
     assert history["mass"][-1]["average"] > 100
+
+
+def test_mass_maximisation_constrained():
+
+    evolver = evo.Evolver(
+        targets={"maximise": ["mass"]},
+        population_size=50,
+        constraints={"percentages": {"Al": {"min": 0.1}}},
+    )
+
+    history = evolver.evolve()
+
+    assert history["mass"][-1]["average"] > 100
+
+    for i, row in history["alloys"].iterrows():
+        assert "Al" in row["alloy"].composition
+        assert row["alloy"].composition["Al"] >= 0.1
